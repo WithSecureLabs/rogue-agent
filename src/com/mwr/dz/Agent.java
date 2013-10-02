80e2e26e59d6329b36383470d70b0033d3bf1fcc
Daniel Bradberry <user.email>
2013-10-02 08:28:14 +0100

    use the same preferences for the rogue agent as the standard; this makes the custom ID more portable

--- src/com/mwr/dz/Agent.java
+++ src/com/mwr/dz/Agent.java
@@ -22,9 +22,13 @@ import android.util.Log;
 
 public class Agent extends Service {
 	
+	private static String[] DEFAULT_UIDS = new String[]{
+		"9774d56d682e549c",
+		"0000000000000000" };
+	
 	private static Agent INSTANCE;
 	
-	private static final String CREATED_UID_KEY = "agent:uid";
+	private static final String AGENT_ID = "agent:uid";
 	
 	private Client client;
 	private DeviceInfo device_info;
@@ -34,46 +38,60 @@ public class Agent extends Service {
 	public static Context getContext() {
 		return Agent.INSTANCE.getMercuryContext();
 	}
+	
 	public Context getMercuryContext() {
 		return this.getApplicationContext();
 	}
 	
-	private void setCustomUID(String uid){
-		if(uid != null && !uid.equals("")){
-			SharedPreferences prefs = this.getSharedPreferences("sysplugin", MODE_PRIVATE);
-			Editor edits = prefs.edit();
-			edits.putString(Agent.CREATED_UID_KEY, uid);
-			edits.commit();
-		}
-	}
-	
-	private String getCustomUID(){
-		SharedPreferences prefs = this.getSharedPreferences("sysplugin", MODE_PRIVATE);
-		String uid = prefs.getString(Agent.CREATED_UID_KEY, null);
+	private static boolean isDefaultUID(String uid) {
+		if(uid == null)
+			return false;
 		
-		return uid;
+		for(String default_uid : Agent.DEFAULT_UIDS) {
+			if(uid.equals(default_uid))
+				return true;
+		}
+		
+		return false;
 	}
 	
 	private String createRandomUID(){
 		SharedPreferences prefs = this.getSharedPreferences("sysplugin", MODE_PRIVATE);
 		uid = new BigInteger(64, new SecureRandom()).toString(32);
 		Editor edits = prefs.edit();
-		edits.putString(Agent.CREATED_UID_KEY, uid);
+		edits.putString(Agent.AGENT_ID, uid);
 		edits.commit();
 		return uid;
 	}
+	
+	private String getCustomUID(){
+		SharedPreferences prefs = this.getSettings();
+		String uid = prefs.getString(Agent.AGENT_ID, null);
+		
+		return uid;
+	}
 
+	public SharedPreferences getSettings() {
+		return this.getSharedPreferences(this.getPackageName() + "_preferences", MODE_PRIVATE);
+	}
 
 	public String getUID() {
 		this.uid = this.getCustomUID();
 		
-		if(this.uid == null || this.uid.equals(""))
-			this.uid = Settings.Secure.getString(this.getMercuryContext().getContentResolver(), Settings.Secure.ANDROID_ID);
-
+		// if the UID is set in preferences, return it immediately
+		if(this.uid != null && !this.uid.equals(""))
+			return this.uid;
+		
+		// otherwise, try to read the ANDROID_ID from Settings.Secure
+		this.uid = Settings.Secure.getString(this.getMercuryContext().getContentResolver(), Settings.Secure.ANDROID_ID);
 		// sometimes, a device will not have an ANDROID_ID, particularly if we
 		// are in lower API versions; in that case we generate one at random
-		if(this.uid == null)
+		if(this.uid == null || Agent.isDefaultUID(this.uid))
 			this.uid = this.createRandomUID();
+		// store whatever UID we have created in the Preferences
+		Editor edit = this.getSettings().edit();
+		edit.putString(Agent.AGENT_ID, this.uid);
+		edit.commit();
 
 		return this.uid;
 	}
@@ -88,8 +106,6 @@ public class Agent extends Service {
 		super.onCreate();
 		
 		Agent.INSTANCE = this;
-		Configuration agent_configuration = new Configuration(this.getApplicationContext(), R.raw.agent_prefs);
-		this.setCustomUID(agent_configuration.get("uid"));
 		
 		Configuration configuration = new Configuration(this.getApplicationContext(), R.raw.endpoint);
 		this.device_info = new DeviceInfo(this.getUID(),
